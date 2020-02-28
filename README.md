88c03dbf4bbc9c5f4fb0a9d68bc3b64805664719
ieggel <ivan.eggel@gmail.com>
2020-02-28 10:56:41 +0100

    working version

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # process-uc1-integration
+
+## docker (folder)
+
+Contains everything needed to build the docker image
+
+## install_mount-fstab-lobcder.sh
+
+Script that installs davfs2, creates mount target dir, creates webdav secrets file for lobcder credentials, creates fstab entry and mounts target dir.
+
+This script is not used in the cloud (or VM), but is rather meant to run locally to mount lobcder.
+
+## process-uc1-execution-script.s
+
+Script that prepares the VM, launches the docker container for the usecase and cleans up resources after termination.
+
+VM preparation:
+- Install davfs2
+- Creates entry for lobcder credentaisl in /etc/davfs2/secrets
+- Creates target mount dir /mnt/lobcder
+- Mounts target mount dir with lobcder
+
+Launch docker container:
+- Mounts /mnt/lobcder/snedtn/camelyon17 from host to /process-uc1/data/camelyon17 in container
+- Run bin/cnn --config-file etc/config.ini extract
+- Results saved to lobcder : snetdn/uc1-results
+
+
+Resource cleanup:
+
+- Unmounts target mount dir (in order to to force cash to be flushed and written to remote resource)
--- process-uc1-execution-script.sh
+++ process-uc1-execution-script.sh
@@ -1,46 +1,64 @@
+
 #!/bin/bash
 
 #NOTES
-#**************************************************************
-# This script does the following:
-# - Install webdav client
-# - Configure lobcder webdav access
-# - Mount lobcder webdav to target mount dir (/mnt/lobcder)
-# - Launch docker container for UC1 (patch extraction)
-# - Unmount mounted lobcder dir
-#**************************************************************
+#******************************************************************************
+# * This script does the following:
+#  - Define lobcder webdav credentials
+#  - Install and configure webdav client
+#  - Mount lobcder webdav to target mount dir (/mnt/lobcder)
+#  - Launch docker container for UC1 (patch extraction)
+#  - Unmount mounted lobcder dir (in order to write cash to remote resource)
+#
+#
+# * Before executing this script you have to provide the lobcder credentails,
+#   by defining values for the following variables:
+#  - WEBDAV_SERVER_URL
+#  - WEBDAV_USERNAME
+#  - WEBDAV_PASSWORD
+#
+# * Script should be called from current directory
+#   - ./process-uc1-execution-script.sh
+#******************************************************************************
+
+#Define lobcder webdav credentials
+#WEBDAV_SERVER_URL=
+#WEBDAV_USERNAME=
+#WEBDAV_PASSWORD=
 
 # Configure autoselection for davfs2 package
-cat <<EOF | debconf-set-selections 
+cat <<EOF | sudo debconf-set-selections 
 davfs2 davfs2/suid_file boolean false 
 EOF
 
 #Install davfs2
-apt install -y davfs2
-
-#Define lobcder webdav credentials
-WEBDAV_SERVER_URL=
-WEBDAV_USERNAME=
-WEBDAV_PASSWORD=
+sudo apt install -y davfs2
 
 #Write credentials to secrets file, so they will not be asked for during execution
 # -q => quiet, -x => match whole line, -F => pattern is plain string
-grep -qxF "${WEBDAV_SERVER_URL} ${WEBDAV_USERNAME} ${WEBDAV_PASSWORD}" /etc/davfs2/secrets || echo "${WEBDAV_SERVER_URL} ${WEBDAV_USERNAME} ${WEBDAV_PASSWORD}">> /etc/davfs2/secrets
+sudo grep -qxF "${WEBDAV_SERVER_URL} ${WEBDAV_USERNAME} ${WEBDAV_PASSWORD}" /etc/davfs2/secrets || echo "${WEBDAV_SERVER_URL} ${WEBDAV_USERNAME} ${WEBDAV_PASSWORD}" | sudo tee -a /etc/davfs2/secrets >/dev/null
+
+#Increase davfs2 cache size to 10GB
+#(davfs2 fully downloads each file that is accessed to a cache dir, if cache size is not big enough, redownload will be triggered indefinitely)
+sudo grep -qxF "cache_size	15000" /etc/davfs2/davfs2.conf || echo "cache_size	15000" | sudo tee -a /etc/davfs2/davfs2.conf
 
 #Change permissions for secrets file (only visible to root)
-chmod 600 /etc/davfs2/secrets
+sudo chmod 600 /etc/davfs2/secrets
 
 #Create target mount dir
-mkdir -p /mnt/lobcder
+sudo mkdir -p /mnt/lobcder
 
 #Mount lobcder via webdav to target mount dir
-mount -t davfs "$WEBDAV_SERVER_URL" /mnt/lobcder
+sudo mount -t davfs "$WEBDAV_SERVER_URL" /mnt/lobcder
+
+#Build Docker image
+docker build -t medgift/process-uc1-patch-extraction ./docker
 
 #--LAUNCH DOCKER CONTAINER
-
+docker run --rm -v /mnt/lobcder/snedtn/camelyon17:/process-uc1/data/camelyon17 -v  /mnt/lobcder/snedtn/uc1-results:/process-uc1/results medgift/process-uc1-patch-extraction
 #-------------------------
 
 #Unmount target mount dir (write cache content)
-
+sudo umount /mnt/lobcder
 
 

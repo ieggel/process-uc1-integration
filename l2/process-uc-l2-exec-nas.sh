52314a0dc404b398f14894eb220c498abdc1728f
ieggel <ivan.eggel@gmail.com>
2020-08-31 11:23:05 +0200

    Add first working version for l2

--- l2/process-uc-l2-exec-nas.sh
+++ l2/process-uc-l2-exec-nas.sh
@@ -2,39 +2,44 @@
 
 #NOTES
 #******************************************************************************
-# SSHFS VERSION
+# NAS VERSION
 #
 # * This script does the following:
-#  - Install and configure sshfs client
-#  - Mount snetdn target mount dir (/mnt/snedtn) via sshfs
-#  - Launch docker container for UC1 (patch extraction)
-#  - Unmount mounted snetdn dir (in order to write cash to remote resource)
-#
-#
-# * Before executing this script you have to make sure that the private key
-#   is placed on the machne that executes this script
+#  - Build & Launch docker container for UC1-L2 (training)
+##
 #
 # * Script should be called from current directory
-#   - ./process-uc1-execution-script-sshfs.sh
+#   - ./process-uc1-l2-exec-nas.sh
 #******************************************************************************
 
 set -euo pipefail
 
 experiment_type="exp-$(date +'%Y-%m-%d_%H_%M_%S')"
 
-
 #Build Docker image
 #Pass random number for REDO_CLONE arg. This triggers a new clone of the git repo
 #where (REDO_CLONE previous)!= (REDO_CLONE_current)
-docker build --build-arg="REDO_CLONE=${RANDOM}" -t medgift/process-uc1-training ./docker
+#docker build --build-arg="REDO_CLONE=${RANDOM}" -t medgift/process-uc1-training ./docker
+docker build --build-arg="REDO_CLONE=${RANDOM}" -t medgift/process-uc1-training -f ./docker/Dockerfile.gpu ./docker
+
+#Define GPU options for Horovod. Those will be passed as an argument to the docker container
+#When using multiple worker nodes, make sure horovod is running on all of those
+#E.g. dockerr run -it --gpus all --network=host -v /mnt/share/ssh:/root/.ssh horovod:latest \
+#    bash -c "/usr/sbin/sshd -p 12345; sleep infinity
+#hvd_opts="-np 7 -H localhost:4,lxhultrafast.hevs.ch:2,lxhevenfaster.hevs.ch:1 -p12345 --start-timeout 300 --verbose"
+hvd_opts="-np 4 -H localhost:4"
 
 #--LAUNCH DOCKER CONTAINER
 docker run \
+--privileged \
+-it \
+--network=host \
 --gpus all \
 --rm \
+-v /mnt/nas2/results/00_TEST-IVAN/horovod-ssh-keys/.ssh:/root/.ssh \
 -v /mnt/nas2/results/00_TEST-IVAN:/results \
 -v /mnt/nas2/results/00_TEST-IVAN/results:/code/PROCESS_L2/results \
 medgift/process-uc1-training \
-$experiment_type
+/bin/bash hvd_train.sh $experiment_type "$hvd_opts"
 #-------------------------
 
